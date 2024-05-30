30cfe4d87395e8a870a6383d81d80694997a89ca
João Lucas <jlmazetti03@hotmail.com>
2024-05-30 15:31:28 -0300

    Ajustes no HTML e inserir botão <LI>

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 //ao clicar na disciplina, seleciona o banco de questoes da disciplina
 
+// Captura todos os elementos <li> dentro da lista pelo seu ID
+const itensLista = document.querySelectorAll('#listaDisciplinas li');
+
+// EVENTO AO CLICAR 
+itensLista.forEach(function(item) {
+    item.addEventListener('click', function() {
+        //GUARDAR ID DA LISTA SELECIONADA
+        let guardaDisciplina = item.innerText;
+        console.log(guardaDisciplina);
+        // Você pode adicionar qualquer ação desejada aqui
+    });
+});
+
 
 // BOTÃO APÓS SELECIONAR A DISCIPLINA QUE DESEJA REALZIAR
 const botaoContinuar = document.getElementById('botaoSim');
