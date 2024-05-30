ad801db5630db234a3c8f849e63c7b7b2eecd2e4
João Lucas <jlmazetti03@hotmail.com>
2024-05-30 15:08:36 -0300

    Atualiando HTML e inserindo botão de confirmar

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//ao clicar na disciplina, seleciona o banco de questoes da disciplina
+
+
+// BOTÃO APÓS SELECIONAR A DISCIPLINA QUE DESEJA REALZIAR
+const botaoContinuar = document.getElementById('botao-sim');
+
+// EVENTO AO CLICAR
+botao-sim.addEventListener('click', function() {
+    // PAGINA DE PERGUNTAS
+    const novaPagina = " ";
+    
+    window.location.href = novaPagina;
+});
